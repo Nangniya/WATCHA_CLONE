ced9419b4a36d07c02d6dfaf8c9eccff1e40411a
김나연 <apinksky00@gmail.com>
2024-11-22 15:11:14 +0900

    chore: babel core, cli 설치 후 실험
    - src/index.js -> dist/index.js에 es5로 변환

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const arrowFunction = function () {
+  return "Hello, Babel!";
+};
+
+const promiseExample = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    return resolve("Promise resolved!");
+  }, 1000);
+});
+promiseExample.then(console.log);
+console.log(arrowFunction());
