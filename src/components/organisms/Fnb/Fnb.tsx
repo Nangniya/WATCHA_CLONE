4fd668c67e250fdf96c3587e28eb288258051152
김나연 <apinksky00@gmail.com>
2024-11-29 18:46:39 +0900

    feat: Fnb 추가

---
+++ src/components/organisms/Fnb/Fnb.tsx
@@ -0,0 +1,5 @@
+const Fnb = () => {
+  return <div>Fnb</div>;
+};
+
+export default Fnb;
--- src/components/templates/Layout/Layout.tsx
+++ src/components/templates/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router';
 import Gnb from '../../organisms/Gnb/Gnb';
+import Fnb from '../../organisms/Fnb/Fnb';
 
 const Layout = () => {
   return (
     <>
       <Gnb />
       <Outlet />
+      <Fnb />
     </>
   );
 };
