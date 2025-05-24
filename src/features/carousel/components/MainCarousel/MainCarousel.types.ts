a8c8b7acc909268067bbfef0acd49b2924aa71c8
김나연 <apinksky00@gmail.com>
2025-05-24 22:06:05 +0900

    chore: DotEnv에 맞게 환경변수 설정 과정 수정

--- src/features/carousel/components/MainCarousel/MainCarousel.types.ts
+++
@@ -1,7 +0,0 @@
-import { IBaseMovieData } from '@/services/movie/movie.types';
-
-export interface IProps {
-  data: IBaseMovieData[] | null;
-}
-
-export type Direction = 'next' | 'prev';
--- src/queries/system.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getNotifications } from '@/services/notification/notification';
-
-export const useNotification = () => {
-  return useQuery({
-    queryKey: ['notification'],
-    queryFn: getNotifications,
-  });
-};
